6141cbb1ee8ae0d51c23422f68b1f8f88b03e1ab
Regalijan <72576136+Regalijan@users.noreply.github.com>
2024-10-25 15:03:10 -0400

    Support setting redirect uri

--- lib/omniauth/strategies/roblox.rb
+++ lib/omniauth/strategies/roblox.rb
@@ -42,6 +42,10 @@ module OmniAuth
       def verified_email
         raw_info['email_verified'] ? raw_info['email'] : nil
       end
+
+      def callback_url
+				options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
